2c6b03ede954d9f625faa23a2563ccc5014765c1
Sergio Mendoza <smendoza787@gmail.com>
2017-08-16 19:04:08 -0700

    changed color scheme, changed appbar title name

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
 	return (
 		<div className="home-page">
 	  	<h1><strong className="main-header">Thirsty? <FontAwesome name="beer"/></strong></h1>
-			<h2 className="main-header">I sure hope so.</h2>
+			<h2 className="main-header"><strong>I sure hope so.</strong></h2>
 			<h3 className="main-subheader"><strong>Choose a bar from the list to see more info</strong></h3>
 		</div>
 	)
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,8 @@ import AppBar from 'material-ui/AppBar';
 const NavBar = () => {
   return (
     <AppBar
-    title="DrinksNow"
-    style={{ backgroundColor: '#2196F3', zIndex: '10', alignItems: 'center', color: '#000' }}
+    title="DrinkUp"
+    style={{ backgroundColor: '#fb3a4c', zIndex: '10', alignItems: 'center', color: '#000' }}
     showMenuIconButton={false} >
       <NavLink
       style={{ marginRight: '10px'}}
